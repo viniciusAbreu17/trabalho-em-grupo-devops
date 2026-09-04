bddc330725c633ae07b051118d3cef0c9ecb260c
Mateus Portes Vilela <165099479+MateusPortesVilela@users.noreply.github.com>
2026-09-04 15:50:07 -0300

    feat: Implementação da validação de formulário e mensagem de sucesso.

--- script.js
+++ script.js
@@ -5,9 +5,25 @@ const resposta = document.querySelector("#resposta");
 botaoDestaque.addEventListener("click", () => {
   servicos.scrollIntoView({ behavior: "smooth" });
 });
-
 formContato.addEventListener("submit", (evento) => {
   evento.preventDefault();
   const nome = document.querySelector("#nome").value;
-  resposta.textContent = `Obrigado pelo contato, ${nome}!`;
+  resposta.textContent = Obrigado pelo contato, ${nome}!;
+});
+
+ // Verifica se algum campo está vazio
+    if (nome === ""  assunto === "" 
+ mensagem === "") {
+        alert("Por favor, preencha todos os campos!");
+        return;
+    }
+
+    // Se todos os campos estiverem preenchidos
+    alert("Formulário enviado com sucesso!");
+
+    // Mostra mensagem na página
+    resposta.textContent = Obrigado pelo contato, ${nome}!;
+
+    // Limpa os campos do formulário
+    formContato.reset();
 });
